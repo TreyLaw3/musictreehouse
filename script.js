10639855d34b4dc307285f36f23a5bba4fedb9c1
TreyLaw3 <107156183+TreyLaw3@users.noreply.github.com>
2022-06-20 22:18:09 +0000

    attached modal and aside for contact

--- script.js
+++ script.js
@@ -3,28 +3,33 @@ const dropdownList = [...dropdownElementList].map(dropdownToggleEl => new bootst
 
 
 
-// Get the modal
-let modal = document.getElementById("mth-contact-info");
+// // Get the modal
+// let modal = document.getElementById("mth-contact-info");
 
-// Get the button that opens the modal
-let btn = document.getElementById("mth-contact-info");
+// // Get the button that opens the modal
+// let btn = document.getElementById("mth-contact-info");
 
-// Get the <span> element that closes the modal
-let span = document.getElementsByClassName("take-a-bow")[0];
+// // Get the <span> element that closes the modal
+// let span = document.getElementsByClassName("take-a-bow")[0];
 
-// When the user clicks on the button, open the modal 
-btn.onclick = function() {
-  modal.style.display = "block";
-}
+// // When the user clicks on the button, open the modal 
+// btn.onclick = function() {
+//   modal.style.display = "block";
+// }
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
+// // When the user clicks on <span> (x), close the modal
+// span.onclick = function() {
+//   modal.style.display = "none";
+// }
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
+// // When the user clicks anywhere outside of the modal, close it
+// window.onclick = function(event) {
+//   if (event.target == modal) {
+//     modal.style.display = "none";
+//   }
+// }
+
+// let
+
+let navbarDropdownMenuLink = document.querySelector("#navbarDropdownMenuLink");
+console.log(navbarDropdownMenuLink);
